a944286c3da19fc7a463b9bce12259ff5f93b573
Naman Gupta <32638824+namangup@users.noreply.github.com>
2021-06-02 00:54:03 +0530

    Update resultCalculator.py

--- codeforces/resultCalculator.py
+++ codeforces/resultCalculator.py
@@ -5,13 +5,15 @@ import sys
 
 #PART 1- input CSV -> string of handles (for parameter of API)
 #and- input CSV -> dictOfHandles
+handles = []
 with open(sys.argv[1], mode='r') as csv_file:
     stringOfHandles = ''
     dictOfHandles = {} #with key = handle; value = roll no.
     csv_reader = csv.reader(csv_file, delimiter=',')
     flag = 0
     for row in csv_reader:
-        lowerCaseHandle = row[1].lower().strip()
+        lowerCaseHandle = row[1].strip()
+        handles.append(lowerCaseHandle)
         if(flag==0):
             stringOfHandles += lowerCaseHandle
             flag=1
@@ -24,7 +26,7 @@ with open(sys.argv[1], mode='r') as csv_file:
 #PART 2- API -> handles in order of their relative ranks 
 parameters= {
     "contestId": sys.argv[2],
-    "handles" : stringOfHandles,
+    # "handles" : stringOfHandles,
     "showUnofficial": 'true'
 }
 URL = "https://codeforces.com/api/contest.standings"
@@ -32,6 +34,13 @@ page = requests.get(URL, params=parameters)
 
 import json
 tp = page.json()['result']['rows']
+
+tpf = []
+for i in tp:
+    if i['party']['members'][0]['handle'] in handles:
+        tpf.append(i)
+
+tp = tpf
 
 handlesInOrder = []
 ranksInOrder = []
@@ -42,7 +51,7 @@ for person in tp:
     members_list = (person['party'])['members']
     handle = (members_list[0])['handle']
     absolute_rank = person['rank']
-    handlesInOrder += [handle.lower()]
+    handlesInOrder += [handle]
     ranksInOrder += [absolute_rank]
 #print(handlesInOrder)
 
